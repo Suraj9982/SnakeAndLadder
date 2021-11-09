f75638812602f66137136d019d27a9b1d6c5c155
suraj9982 <surajsk9982@gmail.com>
2021-11-09 22:34:04 +0530

    [suraj] add ; condition addded reapeat until 100

--- SnakeAndLadderGame/SnakeAndLadderGame/SnakeLadder.cs
+++ SnakeAndLadderGame/SnakeAndLadderGame/SnakeLadder.cs
@@ -8,7 +8,7 @@ namespace SnakeAndLadderGame
 {
     class SnakeLadder
     {
-        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
+        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2,WIN=100,START_POINT=0;
         public int Player_Position = 0;
         Random random = new Random();
         public int Dieroll()
@@ -18,22 +18,29 @@ namespace SnakeAndLadderGame
         }
         public void GamePlay()
         {
-            int dieroll = this.Dieroll();
-            int option = random.Next(0, 3);
-            switch (option)
+            while (Player_Position < 100)
             {
-                case NO_PLAY:
-                    this.Player_Position += NO_PLAY;
-                    break;
-                case LADDER:
-                    this.Player_Position += dieroll;
-                    break;
-                case SNAKE:
-                    this.Player_Position -= dieroll;
-                    break;
-                default:
-                    Console.WriteLine("dicenumber should be between 1 to 6");
-                    break;
+                int dienumber = this.Dieroll();
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case NO_PLAY:
+                        this.Player_Position += NO_PLAY;
+                        break;
+                    case LADDER:
+                        this.Player_Position += dienumber;
+                        break;
+                    case SNAKE:
+                        this.Player_Position -= dienumber;
+                        if (Player_Position < START_POINT)
+                        {
+                            this.Player_Position = START_POINT;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("dicenumber should be between 1 to 6");
+                        break;
+                }
             }
             Console.WriteLine("player position is " + this.Player_Position);
         }
