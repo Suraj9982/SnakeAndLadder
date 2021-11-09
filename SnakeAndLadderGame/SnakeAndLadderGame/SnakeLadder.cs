7857c6129c3fb2d2f74b80b56bed849ea3ba1dc9
suraj9982 <surajsk9982@gmail.com>
2021-11-09 21:12:24 +0530

    [suraj] add : playerpostion start with 0

---
+++ SnakeAndLadderGame/SnakeAndLadderGame/SnakeLadder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame
+{
+    class SnakeLadder
+    {
+        public int Player_Position = 0;
+    }
+}
