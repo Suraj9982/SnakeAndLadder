1cfcbdd824abca839424e9795c4ad96a681e6032
suraj9982 <surajsk9982@gmail.com>
2021-11-09 22:02:27 +0530

    [suraj] add : check player will noplay ladder or snake

--- SnakeAndLadderGame/SnakeAndLadderGame/SnakeLadder.cs
+++ SnakeAndLadderGame/SnakeAndLadderGame/SnakeLadder.cs
@@ -8,12 +8,34 @@ namespace SnakeAndLadderGame
 {
     class SnakeLadder
     {
+        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
         public int Player_Position = 0;
         Random random = new Random();
-        public int dieroll()
+        public int Dieroll()
         {
             int dienumber = random.Next(1, 7);
             return dienumber;
+        }
+        public void GamePlay()
+        {
+            int dieroll = this.Dieroll();
+            int option = random.Next(0, 3);
+            switch (option)
+            {
+                case NO_PLAY:
+                    this.Player_Position += NO_PLAY;
+                    break;
+                case LADDER:
+                    this.Player_Position += dieroll;
+                    break;
+                case SNAKE:
+                    this.Player_Position -= dieroll;
+                    break;
+                default:
+                    Console.WriteLine("dicenumber should be between 1 to 6");
+                    break;
+            }
+            Console.WriteLine("player position is " + this.Player_Position);
         }
     }
 }
