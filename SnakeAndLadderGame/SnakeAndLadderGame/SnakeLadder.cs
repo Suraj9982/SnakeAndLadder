d1d32ec32063b862b370ad907e180c5bdfb555af
suraj9982 <surajsk9982@gmail.com>
2021-11-12 14:06:02 +0530

    [suraj] add : rectify variable name and method name

--- SnakeAndLadderGame/SnakeAndLadderGame/SnakeLadder.cs
+++ SnakeAndLadderGame/SnakeAndLadderGame/SnakeLadder.cs
@@ -8,9 +8,9 @@ namespace SnakeAndLadderGame
 {
     class SnakeLadder
     {
-        public int Player_Position = 0;
+        public int player_position = 0;
         Random random = new Random();
-        public int dieroll()
+        public int Dieroll()
         {
             int dienumber = random.Next(1, 7);
             return dienumber;
